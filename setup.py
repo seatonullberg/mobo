895ac8f0d094b84841087ccd409a4848a0efa65b
seatonullberg <seatonullberg@gmail.com>
2018-09-13 17:33:33 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mobo',
+      version='0.1',
+      description='Multiple Objective Optimization',
+      url='https://github.com/seatonullberg/mobo',
+      author='R. Seaton Ullberg',
+      author_email='seatonullberg@ufl.edu',
+      license='BSD 2-Clause License',
+      packages=['mobo'],
+      zip_safe=True)
