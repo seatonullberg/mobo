b62fa6e8d681db0516cd69da6b7e84a427e74764
seatonullberg <sullberg@ufl.edu>
2019-11-13 14:16:19 -0500

    added logging

--- mobo/optimize.py
+++ mobo/optimize.py
@@ -15,17 +15,21 @@ class Optimizer(object):
         self.configuration = configuration
 
     def __call__(self) -> None:
+        self._log("Beginning the optimization process...")
         # initialize the dataframe
         path = self.configuration.initial_data_path
         if path is None:
+            self._log("Generating initial parameter distributions...")
             index = range(self.configuration.n_samples)
             df = pd.DataFrame(columns=self.df_column_names, index=index)
             init_dist = self._generate_initial_parameter_distributions()
             df[self.parameter_names] = init_dist
         else:
+            self._log("Reading initial parameter distributions from file...")
             df = pd.read_csv(path)
         # loop over each iteration
         for i in range(self.configuration.n_iterations):
+            self._log("\nBeginning iteration {}...".format(i))
             df = self._evaluate(df, i) # generate errors
             df = self._filter(df, i)   # remove poor parameterizations
             df = df.reset_index()
@@ -33,6 +37,7 @@ class Optimizer(object):
             df = self._cluster(df, i)  # cluster projected parameters
             df = self._export(df, i)   # write iteration data to file
             df = self._sample(df, i)   # generate new parameter distribution
+            self._log("Completed iteration {}\n".format(i))
 
     @property
     def parameter_names(self) -> List[str]:
@@ -63,6 +68,7 @@ class Optimizer(object):
 
     def _evaluate(self, df: pd.DataFrame, iteration: int) -> pd.DataFrame:
         """Evaluate each qoi for each parameterization."""
+        self._log("Evaluating parameterizations...")
         err_calc = self.configuration.local_configurations[iteration].error_calculator
         qoi_targets = np.array([
             qoi.target for qoi in self.configuration.qois
@@ -89,14 +95,18 @@ class Optimizer(object):
 
     def _filter(self, df: pd.DataFrame, iteration: int) -> pd.DataFrame:
         """Filter out poor parameterizations."""
+        self._log("Filtering parameterizations...")
+        self._log("\tSamples before filtration: {}".format(len(df)))
         filters = self.configuration.local_configurations[iteration].filters
         for f in filters:
             mask = f(df[self.error_names].to_numpy())
             df = df[mask]
+        self._log("\tSamples after filtration: {}".format(len(df)))
         return df
 
     def _project(self, df: pd.DataFrame, iteration: int) -> pd.DataFrame:
         """Project parameter space down to a 2D space."""
+        self._log("Projecting parameter space to 2D...")
         proj = self.configuration.local_configurations[iteration].projector
         proj_arr = proj(df[self.parameter_names].to_numpy())
         df.update(
@@ -106,6 +116,7 @@ class Optimizer(object):
 
     def _cluster(self, df: pd.DataFrame, iteration: int) -> pd.DataFrame:
         """Assign cluster ids to projected parameters."""
+        self._log("Clustering projected parameter space...")
         clust = self.configuration.local_configurations[iteration].clusterer
         cluster_ids = clust(df[self.projection_names].to_numpy())
         # this is an exception because cluster_names is really just on name
@@ -117,17 +128,23 @@ class Optimizer(object):
     def _export(self, df: pd.DataFrame, iteration: int) -> pd.DataFrame:
         """Export the results of an iteration as a csv file."""
         filename = "mobo_iteration_{}.csv".format(iteration)
+        self._log("Exporting iteration data to {}...".format(filename))
         df.to_csv(filename)
         return df
 
     def _sample(self, df: pd.DataFrame, iteration: int) -> pd.DataFrame:
         """Resample the filtered distribution."""
+        self._log("Resampling parameter space via KDE...")
         cluster_ids = set(df[self.cluster_names].to_numpy().flatten())
         n_samples = self.configuration.local_configurations[iteration].n_samples
         n_samples_per_cluster = n_samples // len(cluster_ids) # TODO
+        self._log(
+            "\tDrawing {} samples per cluster...".format(n_samples_per_cluster)
+        )
         samples = []
         for cluster_id in cluster_ids:
             data = df[df[self.cluster_names[0]] == cluster_id] # another issue caused by single element list
+            self._log("\tCluster {} has {} samples.".format(cluster_id, len(data)))
             data_arr = data[self.parameter_names].to_numpy(float)
             # i will never understand the double transpose
             kde = gaussian_kde(data_arr.T)
@@ -140,6 +157,7 @@ class Optimizer(object):
         df.update(
             {pn: samples_arr[:, i] for i, pn in enumerate(self.parameter_names)}
         )
+        self._log("\tResampled data has {} samples.".format(len(df)))
         return df
 
     def _generate_initial_parameter_distributions(self) -> np.ndarray:
@@ -148,3 +166,11 @@ class Optimizer(object):
         highs = np.array([p.upper_bound for p in self.configuration.parameters])
         size = (self.configuration.n_samples, lows.shape[0])
         return np.random.uniform(low=lows, high=highs, size=size)
+
+    def _log(self, msg: str) -> None:
+        """Log a message to file or stdout."""
+        logger = self.configuration.logger
+        if logger is None:
+            print(msg)
+        else:
+            logger.log(msg)
